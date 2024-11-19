dc5fad21d22caf9efb4c6b82252661652bf1d0a6
Indy Iri <indy.iri@student.pxl.be>
2024-11-19 21:53:04 +0100

    WPF gekopieerd van H3 oef7

---
+++ H11Oef9Lotto2/H11Oef9Lotto2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H11Oef9Lotto2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
